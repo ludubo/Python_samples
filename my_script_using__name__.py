b280254391ce5c0c999c6b346766869dbb1ae942
ludubo <ludubo@gmail.com>
2019-02-11 10:00:01 -0300

    Add files via upload
    Sample of using __name__

---
+++ my_script_using__name__.py
@@ -0,0 +1,15 @@
+# from Jeff Hale, towardsdatascience.com
+#print(__name__)
+# my_script
+print(f"My __name__ is: {__name__}")
+
+def i_am_main():
+    print("I'm main!")
+
+def i_am_imported():
+    print("I'm imported!")
+
+if __name__ == "__main__":
+    i_am_main()
+else:
+    i_am_imported()
